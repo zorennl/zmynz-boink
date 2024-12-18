ced1ce124a0ea427d9aadf7258533e259effc6d6
zorennl <zoren@leit.so>
2024-12-17 18:18:23 -0800

    adding a screen

--- rust/hello_world/target/debug/deps/hello_world-df853cdafd9ed411.d
+++
@@ -1,5 +0,0 @@
-/home/zmynz/vss/git-repo/zmynz-boink/rust/hello_world/target/debug/deps/hello_world-df853cdafd9ed411: src/main.rs
-
-/home/zmynz/vss/git-repo/zmynz-boink/rust/hello_world/target/debug/deps/hello_world-df853cdafd9ed411.d: src/main.rs
-
-src/main.rs:
--- rust/hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/home/zmynz/vss/git-repo/zmynz-boink/rust/hello_world/target/debug/hello_world: /home/zmynz/vss/git-repo/zmynz-boink/rust/hello_world/src/main.rs
