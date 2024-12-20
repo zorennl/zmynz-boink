5ea0dded2cf4757228e8e5550f2df0f5d541651d
BoinkGit <rmelyle@gmail.com>
2024-12-20 17:55:19 -0500

    fixed babygame fire kinda

--- python/babygame/stop_the_baby_fullgame.py
+++ python/babygame/stop_the_baby_fullgame.py
@@ -3,13 +3,14 @@ from time import sleep
 import math
 
 
-fire = open('assets/fire.txt', 'r')
+fire = open('python\\babygame\\assets\\fire.txt', 'r')
+fire = fire.read()
 screenWidth = 800
 screenHeight = 450
 rx = screenWidth
 ry = 200
 bb = "BABY"
-
+print(fire)
 init_window(screenWidth, screenHeight, "Stop the baby v2")
 while not window_should_close():
     sleep(.004)
@@ -20,6 +21,7 @@ while not window_should_close():
     ry = int(200 + 25 * math.sin(rx / 40))
     draw_rectangle(0, 0, screenWidth, screenHeight, DARKGRAY)
     draw_rectangle(0, 150, screenWidth, 150, GRAY)
-    draw_text(str(fire), 20, 200, 10, RED)
+    draw_text(fire, 20, 200, 50, RED)
+    draw_text(bb, int(rx), ry, 50, LIME)
     end_drawing()
 close_window()
