429584726c48a3d9888561961f48540845bb0e8e
BoinkGit <rmelyle@gmail.com>
2024-12-19 20:42:19 -0500

    adding python file because im not incompetent like this goober keeps saying

---
+++ python/epic program so u stop getting on my back on not commiting to the repo.py
@@ -0,0 +1,19 @@
+a = input("What is your favorite color?")
+a = a.lower
+if a == "pink":
+    print("correct answer")
+elif a == "purple":
+    print("correct answer")
+elif a == "blue":
+    print("correct answer")
+elif a == "green":
+    print("moss answer")
+elif a == "black":
+    print("are you zmyz")
+else: 
+    print("i hate you")
+    a = 5
+    while a <= 5:
+        a += 1
+        print(f"these are the letters of the alphabet: {a}")
+    quit()
