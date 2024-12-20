67fb7c2226e1d0ead404bcc21c2cfa8fc90cf79d
zorennl <zoren@leit.so>
2024-12-20 14:30:09 -0800

    testing pyray

--- python/gif_remake/src/main.py
+++ python/gif_remake/src/main.py
@@ -7,6 +7,10 @@ rx = screenWidth
 ry = 200
 
 init_window(screenWidth, screenHeight, "Hello")
+set_exit_key(KEY_Q)
+snailpng = load_image('/assets/snail.png')
+snail = load_texture_from_image(snailpng)
+unload_image(snailpng)
 while not window_should_close():
     sleep(.004)
     begin_drawing()
@@ -15,8 +19,10 @@ while not window_should_close():
     rx -= 1
     if rx < -50:
         rx = screenWidth 
-    draw_rectangle(rx, ry, 50, 50, RED)
+    #draw_rectangle(rx, ry, 50, 50, RED)
+    draw_texture(snail, rx, ry, RED)
     draw_rectangle(0, 250, screenWidth, 225, GREEN)
     end_drawing()
+unload_texture(snail)
 close_window()
 
