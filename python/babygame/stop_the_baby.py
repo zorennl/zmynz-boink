4dc49fe0022a3a96bdf9618c75cd49fb1d747b54
BoinkGit <rmelyle@gmail.com>
2024-12-19 23:40:12 -0500

    S.T.B. update 1.1, it might work, it might not

--- python/babygame/stop_the_baby.py
+++ python/babygame/stop_the_baby.py
@@ -1,11 +1,21 @@
-all_frames = open('assets/frames', 'r')
+from keyboard import read_key as rk
+from time import sleep
+
+all_frames = open('python\\babygame\\assets\\frames.txt', 'r')
 frames = []
 
 for line in all_frames:
     frames.append(line.split()[0])
 
 a = input("ARE YOU READY (Y/N)"); a = a.lower()
+cframe=0 #current_frame
 
 while a == "y":
-    print("- - - - - - - - - - - -(BABY)")
-    x = input
+    print(frames[cframe])
+    key = rk()
+    if rk("a"):
+        print("nice")
+        if cframe > 6:
+            cframe = 3
+    cframe += 1
+    sleep(.2)
