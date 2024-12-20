82a0e86071a94e4cabaa4fa5eaf81861a8d9e290
BoinkGit <rmelyle@gmail.com>
2024-12-19 23:44:22 -0500

    deleting uselss stuff asp

--- rust/weeks/target/debug/deps/input_test-52ccf5bc70c68d94.d
+++
@@ -1,5 +0,0 @@
-/home/zmynz/vss/git-repo/zmynz-boink/rust/weeks/target/debug/deps/input_test-52ccf5bc70c68d94: src/main.rs
-
-/home/zmynz/vss/git-repo/zmynz-boink/rust/weeks/target/debug/deps/input_test-52ccf5bc70c68d94.d: src/main.rs
-
-src/main.rs:
