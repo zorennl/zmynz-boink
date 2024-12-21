a8f45c19ac9d75da5d21014973261d55bb4e5abc
zorennl <zoren@leit.so>
2024-12-20 17:41:26 -0800

    platformer

--- python/platformer/src/main.py
+++ python/platformer/src/main.py
@@ -1,12 +1,33 @@
+from pyray import *
+from time import sleep
+
+class player:
+    def __init__(self, x, y, grav):
+        self.x = x 
+        self.y = y 
+        self.grav = grav
+
+player1 = player(0, 0, 1)
+
+init_window(800, 450, "Hello")
+while not window_should_close():
+    begin_drawing()
+    sleep(0.014)
+    clear_background(WHITE)
     # right
     if is_key_down(KEY_D): 
-        rx += 2
+        player1.x += 2
     # left
     if is_key_down(KEY_A): 
-        rx -= 2
+        player1.x -= 2
     # up
     if is_key_down(KEY_W): 
-        ry -= 2
+        player1.y -= 2
     # down
     if is_key_down(KEY_S):
-        ry += 2    
+        player1.y += 2     
+
+    draw_rectangle(player1.x, player1.y, 50, 50, RED)
+    draw_text("Hello world", 190, 200, 20, BLACK)
+    end_drawing()
+close_window()
