5ec340bc494e66c5ddf746233f1baee4ec59bd66
BoinkGit <rmelyle@gmail.com>
2024-12-20 20:48:08 -0500

    changed keyboard for babygame

--- python/babygame/src/stop_the_baby_fullgame.py
+++ python/babygame/src/stop_the_baby_fullgame.py
@@ -1,17 +1,6 @@
 from pyray import *
 from time import sleep
-from pynput import keyboard
 import math
-
-press = ""
-def on_press(key):
-    global press
-    try: press = key.char
-    except AttributeError: press = key
-
-listener = keyboard.Listener(
-    on_press=on_press)
-listener.start()
 
 fire = open('python\\babygame\\assets\\fire', 'r')
 fire = fire.read()
@@ -20,6 +9,8 @@ screenHeight = 450
 rx = screenWidth
 ry = 200
 bb = "BABY"
+push_timer = 0
+
 print(fire)
 init_window(screenWidth, screenHeight, "Stop the baby v2")
 while not window_should_close():
@@ -30,10 +21,12 @@ while not window_should_close():
     if rx < 30: bb = "DEAD BABY"
     else: rx -= .2
     ry = int(200 + 25 * math.sin(rx / 40))
-
-    if press == keyboard.Key.space:
-        press = ""
+## keyboard
+    if is_key_down(KEY_SPACE):
         rx += 10
+## timer
+    if push_timer > 0:
+        push_timer -= 1
 ## backround
     draw_rectangle(0, 0, screenWidth, screenHeight, DARKGRAY)
     draw_rectangle(0, 150, screenWidth, 150, GRAY)
@@ -42,7 +35,10 @@ while not window_should_close():
 ## baby
     draw_text(bb, int(rx), ry, 50, LIME)
 ## stats
+    press = get_key_pressed()
+    print(press)
     draw_text(f'button: {press}', 10, 10, 20, BLACK)
     draw_text(f'x: {rx}, y: {ry}', 10, 40, 20, BLACK)
+    draw_text(f'push_timer: {push_timer}', 10, 70, 20, BLACK)
     end_drawing()
 close_window()
