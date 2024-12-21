8c0e9ae6b42769231ec26cbb3a7cdb8a341d733e
zorennl <zoren@leit.so>
2024-12-20 17:09:13 -0800

    adding controls

--- python/gif_remake/src/main.py
+++ python/gif_remake/src/main.py
@@ -8,21 +8,29 @@ ry = 200
 
 init_window(screenWidth, screenHeight, "Hello")
 set_exit_key(KEY_Q)
-snailpng = load_image('/assets/snail.png')
-snail = load_texture_from_image(snailpng)
-unload_image(snailpng)
 while not window_should_close():
     sleep(.004)
     begin_drawing()
-    clear_background(WHITE)
+    clear_background(BLUE)
     draw_text("Hello world", 20, 20, 10, GRAY)
-    rx -= 1
-    if rx < -50:
-        rx = screenWidth 
-    draw_rectangle(rx, ry, 50, 50, RED)
-    #draw_texture(snail, rx, ry, RED)
+    # rx -= 1
+    # if rx < -50:i
+        # rx = screenWidth
+    # right
+    if is_key_down(KEY_D): 
+        rx += 2
+    # left
+    if is_key_down(KEY_A): 
+        rx -= 2
+    # up
+    if is_key_down(KEY_W): 
+        ry -= 2
+    # down
+    if is_key_down(KEY_S):
+        ry += 2    
     draw_rectangle(0, 250, screenWidth, 225, GREEN)
+    draw_rectangle(rx, ry, 50, 50, RED)
+
     end_drawing()
-unload_texture(snail)
 close_window()
 
--- python/test.py
+++
@@ -1,4 +0,0 @@
-from pynput.mouse import Button, Controller
-mouse.position = (10, 20)
-print('Now we have moved it to {0}'.format(
-    mouse.position))
