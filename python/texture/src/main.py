3a8227712a30443ba2d19ab97c2b6c463b75cb35
BoinkGit <rmelyle@gmail.com>
2024-12-21 00:42:03 -0500

    fixed texture for windows/relative

--- python/texture/src/main.py
+++ python/texture/src/main.py
@@ -2,7 +2,7 @@ from pyray import *
 
 init_window(800, 450, "texture")
 set_target_fps(60)
-texture = load_texture("/home/zmynz/vss/git-repo/zmynz-boink/python/texture/assets/sprite.png")
+texture = load_texture("python\\texture\\assets\\sprite.png")
 while not window_should_close():
     begin_drawing()
     draw_texture(texture, 50, 50, WHITE)
