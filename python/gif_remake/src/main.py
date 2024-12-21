64113046a62bfe858bfe2966f5b1c454b7eda494
zorennl <zoren@leit.so>
2024-12-20 17:15:05 -0800

    seperating controls from gif remake

--- python/gif_remake/src/main.py
+++ python/gif_remake/src/main.py
@@ -13,21 +13,9 @@ while not window_should_close():
     begin_drawing()
     clear_background(BLUE)
     draw_text("Hello world", 20, 20, 10, GRAY)
-    # rx -= 1
-    # if rx < -50:i
-        # rx = screenWidth
-    # right
-    if is_key_down(KEY_D): 
-        rx += 2
-    # left
-    if is_key_down(KEY_A): 
-        rx -= 2
-    # up
-    if is_key_down(KEY_W): 
-        ry -= 2
-    # down
-    if is_key_down(KEY_S):
-        ry += 2    
+    rx -= 1
+    if rx < -50:
+        rx = screenWidth
     draw_rectangle(0, 250, screenWidth, 225, GREEN)
     draw_rectangle(rx, ry, 50, 50, RED)
 
---
+++ python/platformer/src/main.py
@@ -0,0 +1,12 @@
+    # right
+    if is_key_down(KEY_D): 
+        rx += 2
+    # left
+    if is_key_down(KEY_A): 
+        rx -= 2
+    # up
+    if is_key_down(KEY_W): 
+        ry -= 2
+    # down
+    if is_key_down(KEY_S):
+        ry += 2    
