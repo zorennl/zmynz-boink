ee229d0a2a544dd4b24708a4dddc35412d9e4672
zorennl <zoren@leit.so>
2024-12-23 20:35:06 -0800

    topdown game update

--- python/topdown-game/src/main.py
+++ python/topdown-game/src/main.py
@@ -1,32 +1,28 @@
 from pyray import *
-from time import sleep
-from sys import platform
 
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 450
+dirtraw = 'assets/dirt.png'
 
-class player:
-    def __init__(self,tint,x,y):
-        self.tint =tint
-        self.x =x
-        self.y =y
+init_window(800, 450, "Hello")
 
-init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "topdown")
-png = 'assets/player.png'
-zoren = player(WHITE, 0, 0)
-spr = load_texture(png)
+# set_config_flags(FLAG_WINDOW_TRANSPARENT)
+dirt = load_texture(dirtraw)
 
-set_config_flags(FLAG_WINDOW_TRANSPARENT)
-
-set_target_fps(60)
-
-while window_should_close():
-
-    # plyrcollision = Rectangle(zoren.x, zoren.x, 8, 8 )
-
-    begin_drawing()
-    clear_background(BLANK)
+while not window_should_close():
     
-    draw_texture(spr, zoren.x, zoren.y, zoren.tint)
+    mx = get_mouse_x()
+    my = get_mouse_y()
+    middlex = mx - 8
+    middley = my - 8
+    if is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+        draw_texture(dirt, middlex, middley, WHITE)
+    
+    begin_drawing()
+    
+    # clear_background(BLACK)
+
+    # draw_circle(mx, my, 20, WHITE)
+    draw_texture(dirt, 0, 0, WHITE)
     
     end_drawing()
+unload_texture(dirt)
+close_window()
