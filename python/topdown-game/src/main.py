770e577d6e667763d67c0f4958a5622e6ad7d038
zorennl <zoren@leit.so>
2024-12-25 14:30:05 -0800

    found new lib for py crossplatform texture loading

--- python/topdown-game/src/main.py
+++ python/topdown-game/src/main.py
@@ -1,33 +1,31 @@
 from pyray import *
-from sys import platform
+from os.path import join 
 
-if platform == "win32":
-    dirtraw = 'python\topdown-game\assets\dirt.png'
-else:
-    dirtraw = 'assets/dirt.png'
+dirtr = join('assets', 'dirt.png')
+playerr = join('assets', 'player.png')
+
+set_config_flags(FLAG_WINDOW_TRANSPARENT)
+
+playerpos = Vector2(400, 450)
 
 init_window(800, 450, "Hello")
 
-# set_config_flags(FLAG_WINDOW_TRANSPARENT)
-dirt = load_texture(dirtraw)
+dirt = load_texture(dirtr)
+player = load_texture(playerr)
 
 while not window_should_close():
     
-    mx = get_mouse_x()
-    my = get_mouse_y()
-    middlex = mx - 8
-    middley = my - 8
-    dirtbox = Rectangle(mx, my, 16, 16)
-    dirtboxdest = Rectangle(mx, my, 160, 160)
+    playerpos = get_mouse_position()
+    playerpos.x -= 40; playerpos.y -= 40
     origin = Vector2(0, 0)
         
     begin_drawing()
     
-    clear_background(BLACK)
+    clear_background(BLANK)
 
-    # draw_circle(mx, my, 20, WHITE)
     draw_texture(dirt, 0, 0, WHITE)
-    draw_texture_pro(dirt, dirtbox, dirtboxdest, origin, 0, WHITE)
+    draw_text(f'mx:{playerpos.x}, my:{playerpos.y}', 320, 0, 20, WHITE)
+    draw_texture_ex(player, playerpos, 0, 10, WHITE)
     
     end_drawing()
 unload_texture(dirt)
