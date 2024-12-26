70208eaea3a536ca4048106e6d624990896fad2b
zorennl <zoren@leit.so>
2024-12-25 21:23:49 -0800

    pong dinal release

--- python/pong/src/main.py
+++ python/pong/src/main.py
@@ -7,7 +7,7 @@ WINDOW_HEIGHT = 450
 win = 0
 lose = 0
 
-#set_config_flags(FLAG_WINDOW_TRANSPARENT+FLAG_WINDOW_TOPMOST+FLAG_WINDOW_UNDECORATED)
+set_config_flags(FLAG_WINDOW_TRANSPARENT+FLAG_WINDOW_TOPMOST+FLAG_WINDOW_UNDECORATED)
 
 # BALL
 
@@ -25,6 +25,10 @@ paddledir = 0
 paddle1 = Rectangle(0, 225, 20, 50)
 paddle2 = Rectangle(780, 225, 20, 50)
 
+# SCORES
+bot = 0
+player =0
+
 init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "")
 
 set_target_fps(60)
@@ -32,21 +36,28 @@ set_target_fps(60)
 while not window_should_close():
 
     if is_key_down(KEY_W):
-        paddle1.y -= 2
+        paddle1.y -= 3
     if is_key_down(KEY_S):
-        paddle1.y += 2
+        paddle1.y += 3
+
+    if paddle1.y < 0:
+        paddle1.y += 3
+    if paddle1.y > WINDOW_HEIGHT - 50:
+        paddle1.y -= 3
     paddle2.y = ball.y - 25
     
     if ball.y >= WINDOW_HEIGHT - 25:
         balldir.y = -1
     if ball.x >= WINDOW_WIDTH - 25:
-        win = 60
+        win = 120
+        player += 1
         ballreset()
         ballspd = 0
     if ball.y <= 0:
         balldir.y = 1
     if ball.x <= 0:
-        lose = 60
+        lose = 120
+        bot += 1
         ballreset()
         ballspd = 0
     
@@ -60,18 +71,22 @@ while not window_should_close():
     if win > 0:
         lose -=1
         draw_text("SCORE!", 375, 0, 20, WHITE)
+    if win or lose == 1:
+        ballspd = 1
     if lose > 0:
         lose -= 1
         draw_text("LOSER", 375, 0, 20, WHITE)
     collision1 = check_collision_recs(paddle1, ball)
     collision2 = check_collision_recs(paddle2, ball)
     if collision1 == True:
-        balldir.x = -1
+        balldir.x = 1
     if collision2 == True:
-        balldir.y = 1
+        balldir.x = -1
     draw_rectangle_rec(paddle2,WHITE)
     draw_rectangle_rec(paddle1, WHITE)
     draw_rectangle_rec(ball, WHITE)
+    draw_text(f'score:{player, bot}', 360, 0, 20, WHITE)
+    draw_rectangle_lines(1, 1, WINDOW_WIDTH - 1, WINDOW_HEIGHT - 1, WHITE)
     
     
     end_drawing()
