296be0cb4433bc721f0b1a1105c2379f58b76262
BoinkGit <rmelyle@gmail.com>
2024-12-26 15:39:19 -0500

    added mid ai to pet

--- python/pet_thing/pet.py
+++ python/pet_thing/pet.py
@@ -1,4 +1,5 @@
 from pyray import *
+import math
 from os.path import join as os
 
 
@@ -17,10 +18,15 @@ yMax = get_monitor_height(mNum)
 
 x = 20
 y = 20
+direction = 0
+state = {"moveType":0,"timer":0}
 
 dog = load_texture(os("python","pet_thing","assets","dog.png"))
+dog2 = load_texture(os("python","pet_thing","assets","dog2.png"))
+dog3 = load_texture(os("python","pet_thing","assets","dog3.png"))
 
-print(xMax)
+newDirection = 0
+
 
 while not window_should_close():
 
@@ -29,7 +35,29 @@ while not window_should_close():
     draw_texture(dog,0,0,WHITE)
     draw_text(f"{x}",0,0,10,BLACK)
     draw_text(f"{y}",30,0,10,BLACK)
+    draw_text(f"{direction}",60,0,10,BLACK)
+    draw_text(f"moveType: {state['moveType']}, timer: {state['timer']}",0,20,10,BLACK)
 
+    if state["timer"] == 0:
+        if get_random_value(1,2) == 1:
+            state["moveType"] = 1
+            state["timer"] = get_random_value(60,120)
+        else:
+            state["moveType"] = 2
+            state["timer"] = 80
+            newDirection = direction - get_random_value(-60,60)
+    elif state["moveType"] == 1:
+        x += math.sin(math.radians(direction))*2
+        y += math.cos(math.radians(direction))*2
+        state["timer"] -= 1
+    elif state["moveType"] == 2:
+        x += math.sin(math.radians(direction))*1
+        y += math.cos(math.radians(direction))*1
+        if newDirection > direction:
+            direction += 1
+        elif newDirection < direction:
+            direction -= 1
+        else: state["timer"] = 0
 
     set_window_position(int(x),int(y))
 
