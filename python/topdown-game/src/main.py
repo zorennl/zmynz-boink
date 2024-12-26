751806b95dfeb2ebf7443c9b99390821fbabc619
zorennl <zoren@leit.so>
2024-12-25 17:15:10 -0800

    raygui is cool

--- python/topdown-game/src/main.py
+++ python/topdown-game/src/main.py
@@ -1,13 +1,6 @@
 from pyray import *
 from os.path import join as os
-class bullet:
-    def __init__(self,shape, velocity, time):
-        self.shape = shape
-        self.velocity = velocity
-        self.time = time
-        
-    def drawbullet(bullet):
-        draw_rectangle_rec(bullet.shape, YELLOW)
+
 dirtr = os('assets', 'dirt.png')
 playerr = os('assets', 'player.png')
 
@@ -17,17 +10,19 @@ playerpos = Vector2(400, 450)
 
 init_window(800, 450, "Hello")
 
+set_target_fps(60)
+
+test = False
+
+button = Rectangle(400, 225, 100, 50)
+
 dirt = load_texture(dirtr)
 player = load_texture(playerr)
 
 while not window_should_close():
 
-    bulshape = Rectangle(playerpos.x, playerpos.y, 5, 10)
-    bul = bullet(bulshape, 2, 999999)
-        
     playerpos = get_mouse_position()
     playerpos.x -= 40; playerpos.y -= 40
-    origin = Vector2(0, 0)
         
     begin_drawing()
 
@@ -35,15 +30,14 @@ while not window_should_close():
         
     clear_background(BLANK)
 
-    if is_mouse_button_pressed(MOUSE_BUTTON_LEFT): 
-        for i in range(1, bul.time):
-            bullet.drawbullet(bul)  
-            bulshape.y -= bul.velocity
-    draw_texture(dirt, 0, 0, WHITE)
+    if gui_button(button, "button"):
+        test = not test
+    if test == True:
+        draw_text("button", 0, 0, 20, WHITE)
+    
     draw_text(f'mx:{playerpos.x}, my:{playerpos.y}', 320, 0, 20, WHITE)
     draw_texture_ex(player, playerpos, 0, 10, WHITE)
     
     
     end_drawing()
-unload_texture(dirt)
 close_window()
