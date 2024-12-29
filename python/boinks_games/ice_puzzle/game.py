a533f668a53f57d0b30dfc56e308eaac7d628c96
BoinkGit <rmelyle@gmail.com>
2024-12-28 22:19:33 -0500

    a tad more neatening, start of ice game

---
+++ python/boinks_games/ice_puzzle/game.py
@@ -0,0 +1,13 @@
+from pyray import *
+
+
+init_window(1200,675,"Ice Game For Plebians Like You!")
+
+while not window_should_close():
+    begin_drawing()
+    clear_background(WHITE)
+
+
+
+    end_drawing()
+close_window()
