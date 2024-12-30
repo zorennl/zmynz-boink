956edcafe31f267037a960f369de7318907c3059
BoinkGit <rmelyle@gmail.com>
2024-12-29 22:06:17 -0500

    adding more debug, fixing camera

--- python/boinks_games/ice_puzzle/game.py
+++ python/boinks_games/ice_puzzle/game.py
@@ -2,12 +2,14 @@ from pyray import *
 
 
 init_window(1200,675,"Ice Game For Plebians Like You!")
+set_target_fps(60)
+
 
 while not window_should_close():
     begin_drawing()
     clear_background(WHITE)
 
-
-
+    movement = (is_key_pressed(68)-is_key_pressed(65),is_key_pressed(83)-is_key_pressed(87))
+    
     end_drawing()
 close_window()
