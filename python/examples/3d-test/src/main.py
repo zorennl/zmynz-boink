dade4d5e8d09ff8f7fe9edb6fae9351170ce3f32
zorennl <zoren@leit.so>
2024-12-29 22:27:50 -0800

    3d wires

--- python/examples/3d-test/src/main.py
+++ python/examples/3d-test/src/main.py
@@ -2,7 +2,9 @@ from pyray import *
 
 init_window(800, 500, "3D")
 
-camera = Camera3D(Vector3(1000, 1000, 1000), Vector3(0, 0, 0), Vector3(0, 1, 0), 90, CAMERA_PERSPECTIVE)
+camerapos = Vector3(1000, 1000, 1000)
+
+camera = Camera3D(camerapos, Vector3(0, 0, 0), Vector3(0, 45, 0), 90, CAMERA_PERSPECTIVE)
 cubepos = Vector3(0,0,0)
 set_target_fps(60)
 
@@ -10,22 +12,35 @@ while not window_should_close():
     begin_mode_3d(camera)
     begin_drawing()
     clear_background(WHITE)
-
     draw_cube(cubepos, 100, 100, 100, BLACK)
+    draw_cube_wires(cubepos, 100, 100, 100,RED)
 
     if is_key_down(KEY_A):
         cubepos.x -= 2
     if is_key_down(KEY_D):
         cubepos.x += 2
     if is_key_down(KEY_W):
-        cubepos.y -= 2
-    if is_key_down(KEY_S):
         cubepos.y += 2
+    if is_key_down(KEY_S):
+        cubepos.y -= 2
     if is_key_down(KEY_Q):
         cubepos.z -= 2
     if is_key_down(KEY_E):
         cubepos.z += 2
-    
-    end_drawing()
+
+    if is_key_down(KEY_G):
+        camerapos.x -= 2
+    if is_key_down(KEY_J):
+        camerapos.x += 2
+    if is_key_down(KEY_Y):
+        camerapos.y -= 2
+    if is_key_down(KEY_H):
+        camerapos.y += 2
+    if is_key_down(KEY_U):
+        camerapos.z -= 2
+    if is_key_down(KEY_I):
+        camerapos.z += 2
     end_mode_3d()
+    draw_grid(10, 1)    
+    end_drawing()
 close_window()
