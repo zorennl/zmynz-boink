82bc0dea327d9e0ad597a2b21e840928964de8f8
zoren <zoren@leit.so>
2025-05-05 08:17:37 -0700

    more bees

--- zaboing_games/bees_and_hornets/src/main.py
+++ zaboing_games/bees_and_hornets/src/main.py
@@ -1,5 +1,12 @@
 from pyray import *
+from os.path import join
 
+# Bees
+bees = [
+    Rectangle(0,0,10,10),
+    Rectangle(10,0,10,10),
+    Rectangle(20,0,10,10)
+]
 # General entity class 
 class Entity:
     def __init__(self, health, sprite=None, source=None, rectangle=Rectangle, color=None, extra=None):
@@ -23,12 +30,14 @@ def draw_entity_rec(entity):
 init_window(300,300,"raylib bees and hornets")
 set_target_fps(60)
 
+atlas = load_texture(join('assets','sprites.png'))
+
 while not window_should_close():
 
     begin_drawing()
     clear_background(BLACK)
 
-
+    draw_texture(sprites,0,0,WHITE)
 
     end_drawing()
 
