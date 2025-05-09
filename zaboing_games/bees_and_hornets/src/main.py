d9951c2c44d8f3c81278c3d958776907dcefdfcb
zoren <zoren@leit.so>
2025-05-08 22:01:06 -0700

    attempting to automated add bee to mineral tracking

--- zaboing_games/bees_and_hornets/src/main.py
+++ zaboing_games/bees_and_hornets/src/main.py
@@ -47,6 +47,12 @@ def draw_entity_sprite(entity):
 def draw_entity_rec(entity):
     draw_rectangle_rec(entity.rectangle,entity.color)
 
+def get_distance(pos1,pos2):
+    dx = pos1.x - pos2.x
+    dy = pos1.y - pos2.y
+    distance = math.sqrt(dx*dx+dy*dy)
+    return int(distance)
+
 def get_direction(pos1,pos2):
     dx = pos1.x - pos2.x
     dy = pos1.y - pos2.y
@@ -66,21 +72,21 @@ else:
 bees = []
 minerals = []
 # Player
-player = Entity(10,2,entity_atlas,entities[0],Rectangle(125,125,25,25),0,WHITE,None)
+player = Entity(10,2,entity_atlas,entities[0],Rectangle(138,138,25,25),0,WHITE,None)
 mineral_counter = 0
 while not window_should_close():
     camera.target = Vector2(player.rectangle.x-138,player.rectangle.y-138)
 
     bee = Entity(10,random.randint(10,15)/10,entity_atlas,entities[1],Rectangle(200,200,15,15),0,WHITE,Vector2(0,0))
-    mineral = Entity(1,.0,entity_atlas,entities[2],Rectangle(random.randint(0,300),random.randint(0,300),10,10),0,WHITE,None)
+    mineral = Entity(1,0,entity_atlas,entities[2],Rectangle(random.randint(-1000,1000),random.randint(-1000,1000),10,10),0,WHITE,Vector2(0,0))
 
-    if is_key_down(KEY_W):
+    if is_key_down(KEY_W) and player.rectangle.y > -1000:
         player.rectangle.y -= player.speed
-    if is_key_down(KEY_S):
+    if is_key_down(KEY_S) and player.rectangle.y < 1000:
         player.rectangle.y += player.speed
-    if is_key_down(KEY_A):
+    if is_key_down(KEY_A) and player.rectangle.x > -1000:
         player.rectangle.x -= player.speed
-    if is_key_down(KEY_D):
+    if is_key_down(KEY_D) and player.rectangle.x < 1000:
         player.rectangle.x += player.speed
 
     if is_key_pressed(KEY_R):
@@ -99,12 +105,23 @@ while not window_should_close():
             mineral_counter += 1
             i.health = 0
             del minerals[minerals.index(i)]
+        for x in bees:
+            i.speed = get_distance(Vector2(i.rectangle.x,i.rectangle.y),Vector2(x.rectangle.x,x.rectangle.y))
+            if i.speed < 100:
+                x.extra = get_direction(Vector2(i.rectangle.x,i.rectangle.y),Vector2(x.rectangle.x,x.rectangle.y))
+            else:
+                x.extra = get_direction(Vector2(player.rectangle.x,player.rectangle.y),Vector2(x.rectangle.x,x.rectangle.y))
+            
 
 
     draw_entity_sprite(player)
     for i in bees:
         draw_entity_sprite(i)
-        i.extra = get_direction(Vector2(player.rectangle.x,player.rectangle.y),Vector2(i.rectangle.x,i.rectangle.y))
+        # for x in minerals:
+        #     if x.speed != 1:
+        #         i.extra = get_direction(Vector2(player.rectangle.x,player.rectangle.y),Vector2(i.rectangle.x,i.rectangle.y))
+        #     else:
+        #         i.extra = get_direction(Vector2(i.rectangle.x,i.rectangle.y),Vector2(x.rectangle.x,x.rectangle.y))
         if i.extra.x < 0:
             i.source.width = -10
         else:
@@ -119,7 +136,7 @@ while not window_should_close():
   
 
     if debug:
-        draw_text(f'bees:{len(bees)}\nminerals:{mineral_counter}',0,0,10,GREEN)
+        draw_text(f'x:{player.rectangle.x} y:{player.rectangle.y}\nbees:{len(bees)}\nminerals:{mineral_counter}',0,0,10,GREEN)
     end_drawing()
 
 close_window()
